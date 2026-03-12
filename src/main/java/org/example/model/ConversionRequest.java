c991090d86c4f75605c7d30f73d9b7223bea8aa8
obemeuche <obemeuchechi@gmail.com>
2026-03-12 23:32:58 +0000

    use lombok

--- src/main/java/org/example/model/ConversionRequest.java
+++ src/main/java/org/example/model/ConversionRequest.java
@@ -1,50 +1,17 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ConversionRequest {
     private UnitCategory category;
     private String sourceUnit;
     private String targetUnit;
     private double value;
-
-    public ConversionRequest() {
-    }
-
-    public ConversionRequest(UnitCategory category, String sourceUnit, String targetUnit, double value) {
-        this.category = category;
-        this.sourceUnit = sourceUnit;
-        this.targetUnit = targetUnit;
-        this.value = value;
-    }
-
-    public UnitCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(UnitCategory category) {
-        this.category = category;
-    }
-
-    public String getSourceUnit() {
-        return sourceUnit;
-    }
-
-    public void setSourceUnit(String sourceUnit) {
-        this.sourceUnit = sourceUnit;
-    }
-
-    public String getTargetUnit() {
-        return targetUnit;
-    }
-
-    public void setTargetUnit(String targetUnit) {
-        this.targetUnit = targetUnit;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-    }
 }
