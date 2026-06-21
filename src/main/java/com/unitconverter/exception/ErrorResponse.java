42243715fbaae5a52dce3843eeda29903315d522
obemeuche <obemeuchechi@gmail.com>
2026-06-21 19:37:04 +0100

    add api spec

--- src/main/java/com/unitconverter/exception/ErrorResponse.java
+++ src/main/java/com/unitconverter/exception/ErrorResponse.java
@@ -1,10 +1,14 @@
 package com.unitconverter.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Error response body")
 public class ErrorResponse {
+
+    @Schema(description = "Human-readable error message", example = "No enum constant com.unitconverter.model.UnitCategory.INVALID")
     private String message;
 }
